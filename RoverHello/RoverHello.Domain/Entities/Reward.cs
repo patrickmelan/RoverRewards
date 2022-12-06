fb56eb34b0150d813635f54ba38337bf0aee561a
patrick melan <patrick.melan@outlook.com>
2022-12-06 12:32:55 -0500

    Added Reward

---
+++ RoverHello/RoverHello.Domain/Entities/Reward.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverHello.Domain.Entities
+{
+    public class Reward
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Point Cost")]
+        public int PointCost { get; set; }
+        public string Description { get; set; }
+        public bool Available { get; set; }
+        public int Count { get; set; }
+    }
+}
--- RoverHello/RoverHello.Infrastructure/Persistence/Migrations/20221206173226_AddedReward.Designer.cs
+++ RoverHello/RoverHello.Infrastructure/Persistence/Migrations/20221206173226_AddedReward.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RoverHello.Infrastructure.Persistence.DbContexts;
 
@@ -11,9 +12,11 @@ using RoverHello.Infrastructure.Persistence.DbContexts;
 namespace RoverHello.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221206173226_AddedReward")]
+    partial class AddedReward
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

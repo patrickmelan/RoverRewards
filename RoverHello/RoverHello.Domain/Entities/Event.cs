4a089210b5c42489e8dc1a33519f5cec1ee04095
patrick melan <patrick.melan@outlook.com>
2022-12-09 11:42:49 -0500

    commit

---
+++ RoverHello/RoverHello.Domain/Entities/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverHello.Domain.Entities
+{
+    internal class Event{
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Points { get; set; }
+        public string Description { get; set; }
+
+    }
+}
